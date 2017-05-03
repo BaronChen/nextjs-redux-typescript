508263e91b5ddf4fd792bc60f2127ed283b353fe
Baron Chen <baron.chen@class.com.au>
2017-05-03 10:48:54 +1000

    remove js file from repo

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const hello_world_component_1 = require("../components/hello-world.component");
-exports.default = () => React.createElement("div", null,
-    React.createElement(hello_world_component_1.default, null));
